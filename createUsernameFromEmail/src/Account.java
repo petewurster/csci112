25357a7fca7b5bffeaeef97ab4b8d7af045884b6
petewurster <pwurste1@student.ccp.edu>
2020-07-08 22:45:54 -0400

    lab2 complete

--- createUsernameFromEmail/src/Account.java
+++ createUsernameFromEmail/src/Account.java
@@ -1,3 +1,14 @@
+/**
+Account.java
+2020-07-08 pWurster
+
+this is a class designed to (eventually) be a self contained account creation module
+password hashes are currently written to file, but the intent is to eventually store these
+values in a relational database along with the rest of the account info (email is intended to be the PK).
+future members will include-> lastActivity, lastPWReset, Account(w/params), and a few other helper
+functions for DB connectivity
+*/
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
@@ -8,18 +19,19 @@ import java.util.Date;
 import javax.swing.*;
 import javax.xml.bind.DatatypeConverter;
 
-
-
 public class Account {
+
+    //output file path
     public static final String OUTFILE = "hashes.txt";
 
-
+    //basic members
     private String userName;
     private String email;
     private String passHash;
     private Date createdOn;
     private boolean locked = false;
 
+    //default constructor
     Account() {
         this.email = acquireEmail();
         this.userName = stripNameFromEmail();
@@ -27,24 +39,35 @@ public class Account {
         this.createdOn = new Date();
         this.passHash = hashPassword(acquirePassword());
         printHashToFile();
-
     }
 
+
+
+    //exit code 88 is to signify intentional kill of the program
     private String grabInput(String message) {
         String raw = JOptionPane.showInputDialog(null,message);
         if (raw == null) System.exit(88);
         return raw;
     }
 
+
+
+    //strips user name from email address
     private String stripNameFromEmail() {
         return this.getEmail().replaceAll("@.+[.]edu", "");
     }
 
+
+
+    //valid email test
     private boolean validEmail(String email) {
         String pattern = "[a-zA-Z0-9]+@[a-zA-Z0-9]+[.]edu";
         return email.matches(pattern);
     }
 
+
+
+    //prompts user for email address
     private String acquireEmail() {
         String message;
         String email;
@@ -55,6 +78,9 @@ public class Account {
         return email;
     }
 
+
+
+    //prompts user for password & confirmation
     private String acquirePassword() {
         String password;
         String confirm;
@@ -69,6 +95,12 @@ public class Account {
     }
 
 
+    /**
+     *applies a 128-bit SecureRandom salt, peppers the password with the toString() value of
+     * the createdOn Date, and then hashes with SHA-512
+     * @param password String
+     * @return _inlineConvertedValue_ this value is a String of the Hex representation of the hash's resulting byte array
+     */
     private String hashPassword(String password) {
         try{
             //generate a random salt
@@ -91,6 +123,9 @@ public class Account {
         return null;
     }
 
+
+
+    //override
     public String toString() {
         return "user:" + this.getUserName() +
                 "\nemail:" + this.getEmail() +
@@ -100,22 +135,34 @@ public class Account {
     }
 
 
-    private void toggleLock() {
+
+    //toggle locked status of Account
+    public void toggleLock() {
         this.locked = !this.locked;
     }
 
+
+
+
+    //update email & (subsequently) userName; ignores updates that are not valid
     public void updateIdentity(String newInfo) {
         this.email = validEmail(newInfo) ? newInfo : this.email;
         this.userName = stripNameFromEmail();
         prompt();
     }
 
+
+
+    //updates hashed password
     public void updatePassword(String newInfo) {
         this.passHash = hashPassword(newInfo);
         printHashToFile();
-
     }
 
+
+
+
+    //appends userName & hashed password to file as key:value pair
     private void printHashToFile() {
         try{
             PrintWriter outputStream = new PrintWriter(new FileOutputStream(new File(OUTFILE), true));
@@ -125,14 +172,11 @@ public class Account {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-
-
-
     }
 
 
 
+    //helper func to inform user of successful changes
     private void prompt() {
         String message = "Welcome " + this.getUserName() + "!";
         if (this.getCreatedOn() != null) message = "Your information has been changed!";
@@ -140,6 +184,9 @@ public class Account {
     }
 
 
+
+
+    //mandatory getters due to all member properties being private
     public String getUserName() {
         return this.userName;
     }
@@ -157,4 +204,4 @@ public class Account {
     }
 
 
-}
+}//end of class
--- createUsernameFromEmail/src/Main.java
+++ createUsernameFromEmail/src/Main.java
@@ -1,4 +1,10 @@
+/*
+Main.java
+2020-07-08 pWurster
 
+controller class demonstrating how Account can work almost totally independently to
+create/manage Account instances
+*/
 public class Main {
     public static void main(String[] args) {
         Account user = new Account();
