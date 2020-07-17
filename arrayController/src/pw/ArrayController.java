37f444a5b826e1ee9bd372411bbaba51696403d2
petewurster <pwurste1@student.ccp.edu>
2020-07-17 19:37:30 -0400

    almost done... needs dox

--- arrayController/src/pw/ArrayController.java
+++ arrayController/src/pw/ArrayController.java
@@ -5,28 +5,35 @@ ArrayController.java
 package pw;
 
 public class ArrayController {
-    public CellPhone[] array;
+    public CellPhone[] array = new CellPhone[0];
 
 
+    //create empty ArrayController
+    ArrayController(){}
+
+    //create an ArrayController from any number of CellPhones
     ArrayController(CellPhone... items) {
-        this.array = new CellPhone[0];
         this.addCellPhone(items);
     }
 
 
 
 
-    //create an ArrayController from any number of CellPhones
+    //add any number of CellPhones to the array
     public void addCellPhone(CellPhone... newCells) {
+        //set new array to length of old array + number of new elements
         CellPhone[] newArray = new CellPhone[newCells.length + this.array.length];
-        for (int i = 0; i < this.array.length; i++) {
-            newArray[i] = this.array[i];
-        }
-        for (int i = 0; i < newCells.length; i ++) {
-            newArray[this.array.length + i] = newCells[i];
-        }
+
+        //loop to fill newArray with old data
+        for (int i = 0; i < this.array.length; i++) newArray[i] = this.array[i];
+
+        //loop to add new data to newArray
+        for (int i = 0; i < newCells.length; i ++) newArray[this.array.length + i] = newCells[i];
+
+        //member property now points to newArray
         this.array = newArray;
     }
+
 
 
 
@@ -36,26 +43,37 @@ public class ArrayController {
         removeCellPhone(this.array.length - 1);
     }
 
-
-
-    //removes target element
+    //overloaded method removes target element
     public void removeCellPhone(int index) {
         //fail-fast
         if (0 > index || index >= this.array.length) return;
 
+        //create newArray with length smaller than old array
         CellPhone[] newArray = new CellPhone[this.array.length -1];
-        int j = 0;
+
+        int n = 0;      //marks index within new array
+
+        //loop thru old array; fill newArray with objects, ignoring target index
         for (int i = 0; i < this.array.length; i++) {
-             if (i != index) {
-                 newArray[j] = this.array[i];
-                 j++;
+            if (i != index) {
+                 newArray[n] = this.array[i];
+                 n++;       //update newArray index location
             }
         }
+        //member property now points to newArray
         this.array = newArray;
     }
 
 
 
+    //search for target element based on model; returns index of 1st
+    // object with a matching model
+    public int searchByModel(String target) {
+        for (int i = 0; i < this.array.length; i++) {
+            if (this.array[i].getModel().equals(target)) return i;
+        }
+        return -1;
+    }
 
 
 
