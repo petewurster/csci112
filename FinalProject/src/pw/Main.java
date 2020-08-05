41d0dfdc2b9c16bcf290fe4e015813855be70d43
petewurster <pwurste1@student.ccp.edu>
2020-08-05 12:03:25 -0400

    final

--- FinalProject/src/pw/Main.java
+++ FinalProject/src/pw/Main.java
@@ -1,31 +1,37 @@
+/**
+ * Main.java
+ * 2020-08-01 pWurster
+ *
+ *
+ * This is the driver program that will be used to conduct research on the time
+ * complexity of various sorting algorithms
+ */
+
 package pw;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
+    //init globals
+    private static Scanner rawInput = new Scanner(System.in);
+    private static String[] message = new String[10];
+
+
+    //this makes running the final project samples require a little less typing
+    private static final int SAMPLE_NUM = 5;
 
     public static void main(String[] args) {
-        int[] testArray = createArrayOfInts(Sorts.options[0]);
-//        testArray = new int[]{2,6,4,9,7,0,3,5,1,10,8};
-//        Sorts.bubbleSort(testArray, true);
-//        Sorts.selectionSort(testArray, true);
-//
-//        Sorts.selectionSort(testArray);
-//        Sorts.insertionSort(testArray, true);
-
-        Sorts.mergeSort(testArray, true);
-//        Sorts.quickSort(testArray);
-//        Sorts.insertionSort(testArray);
-        for (int x: testArray) System.out.println(x);
-
-
-
-
+        //start the menu
+        menuLoop();
     }
 
 
 
-    public static int[] createArrayOfInts(int length) {
+
+
+    //creates an array of ints
+    private static int[] createArrayOfInts(int length) {
         Random rnd = new Random();
         int[] array = new int[length];
         for (int i = 0; i < length; i++) array[i] = rnd.nextInt();
@@ -34,6 +40,189 @@ public class Main {
 
 
 
+
+
+
+
+    //runs the chosen test with sampleSize length arrays
+    private static void runTests(int choice, int sampleSize) {
+
+        //capture input regarding number of samples
+        int sample;
+        try {
+            message = new String[]{"How many times do you wanna run this test?"};
+            sample = Integer.parseInt(grabInput(message));
+        } catch (Exception e) {
+            showMessage(new String[]{"*** Using default ***"});
+            sample = SAMPLE_NUM;
+        }
+
+        //prompt user
+        showMessage(new String[]{"", "Running " + sample + " sets of " +
+                Sorts.TESTS[Math.abs(choice)], "on " + Sorts.SIZES[sampleSize] +
+                " elements" + (choice < 0 ? " (reversed)" : "")});
+
+        //perform the test once for each sample
+        for (int i = 1; i < sample + 1; i++) {
+            //this makes the results easier to read, but interferes with copy/paste efforts
+//            System.out.printf("%3d : ", i);
+
+            //instantiate a new array of random ints
+            int[] testArray = createArrayOfInts(Sorts.SIZES[sampleSize]);
+
+            //capture timestamp
+            double time = System.nanoTime();
+
+            //initiate chosen test routine
+            switch (choice) {
+                case 1:
+                    Sorts.bubbleSort(testArray);
+                    break;
+                case -1:
+                    Sorts.bubbleSort(testArray, true);
+                    break;
+                case 2:
+                    Sorts.selectionSort(testArray);
+                    break;
+                case -2:
+                    Sorts.selectionSort(testArray, true);
+                    break;
+                case 3:
+                    Sorts.insertionSort(testArray);
+                    break;
+                case -3:
+                    Sorts.insertionSort(testArray, true);
+                    break;
+                case 4:
+                    Sorts.mergeSort(testArray);
+                    break;
+                case -4:
+                    Sorts.mergeSort(testArray, true);
+                    break;
+                case 5:
+                    Sorts.quickSort(testArray);
+                    break;
+                default: //case -5
+                    Sorts.quickSort(testArray, true);
+
+            }
+
+            //calculate completion time in seconds
+            time = (System.nanoTime() - time) / 1000000000.0;
+            System.out.println(time);
+
+            //stop further testing after time limit is exceeded for the first time
+            if (time > Sorts.TIME_LIMIT) {
+                showMessage(new String[]{"\n***     Tests aborted!     ***",
+                        "*** Exceeded " + Sorts.TIME_LIMIT + " seconds ***\n"});
+                break;
+            }
+
+        }//end for loop
+
+
+    }
+
+
+
+
+
+    //menu loop
+    private static void menuLoop() {
+        while (true) {
+            try {
+                message = getMenu();
+                int choice = Integer.parseInt(grabInput(message));
+
+                //quit if selection out of bounds
+                if (choice < -5 || choice > 5 || choice == 0) exit();
+
+                message = getSampleSize();
+                int sampleSize = Integer.parseInt(grabInput(message));
+
+                //quit if selection out of bounds
+                if (sampleSize < 1 || sampleSize > 8) exit();
+
+                //run selected test SAMPLE_NUM times on sampleSize elements
+                runTests(choice, sampleSize);
+
+            } catch (Exception e) {
+                showMessage(new String[]{"*** Invalid entry ***"});
+            }
+        }
+    }
+
+
+
+
+
+
+    //main menu
+    private static String[] getMenu() {
+        return new String[]{
+                "\n-------------------------",
+                "  Test which algorithm?",
+                "-------------------------",
+                "[1] bubble sort     [-1]",
+                "[2] selection sort  [-2]",
+                "[3] insertion sort  [-3]",
+                "[4] merge sort      [-4]",
+                "[5] quick sort      [-5]",
+                "[6] Quit",
+                "\n( negative option reverses the sort )"
+        };
+    }
+
+
+
+
+
+
+    //secondary menu
+    private static String[] getSampleSize() {
+        return new String[]{
+                "\n-------------------------",
+                "How many random integers?",
+                "-------------------------",
+                "[1] 10000",
+                "[2] 20000",
+                "[3] 100000",
+                "[4] 200000",
+                "[5] 1000000",
+                "[6] 2000000",
+                "[7] 10000000",
+                "[8] 20000000",
+                "[9] Quit"
+        };
+    }
+
+
+
+
+
+    //grab user input for menu after prompting with message
+    private static String grabInput(String[] message) {
+        showMessage(message);
+        return rawInput.nextLine();
+    }
+
+
+
+
+
+    //show message line by line
+    private static void showMessage(String[] message) {
+        for (String line: message) System.out.println(line);
+    }
+
+
+
+
+    //exit code 88 is to signify intentional kill of the program
+    private static void exit() {
+        rawInput.close();
+        System.exit(88);
+    }
 
 
 
--- FinalProject/src/pw/Sorts.java
+++ FinalProject/src/pw/Sorts.java
@@ -1,58 +1,72 @@
 /**
  * Sorts.java
- * 2020-207-28 pWurster
+ * 2020-07-28 pWurster
  *
  *
- * This program holds my implementations of several sorting methods:
+ * This program holds my implementations of several in-place sorting methods:
  * bubbleSort, insertionSort, selectionSort, mergeSort, quickSort
  *
  * These implementations only work for integers, but serve as a skeleton for
- * custom object sorting implementations
+ * custom object sorting implementations.  Reverse order is done by flipping
+ * internal logic of the methods rather than re-copying the array backwards
  *
  */
 package pw;
 
 
 public abstract class Sorts {
+    //class constants
+    public static final double TIME_LIMIT = 300.0; //in seconds
+    public static final String[] TESTS = new String[]{"", "bubble sort", "selection sort", "insertion sort", "merge sort", "quick sort" };
+    public static final int[] SIZES = new int[]{0, 10000, 20000, 100000, 200000, 1000000, 2000000, 10000000, 20000000};
 
 
-    public static final int[] options = new int[]{10000, 20000, 100000, 200000, 1000000, 2000000, 10000000, 20000000};
 
+    /**
+     * bubbleSort -- with early escape
+     *
+     * @param array
+     * @param reverse [optional]
+     */
+     public static void bubbleSort(int[] array, boolean reverse) {
+         //boolean flag allows shortcut termination if sort is completed before full iteration
+         boolean swapped = true;
 
-    //bubbleSort with early escape
-    public static void bubbleSort(int[] array, boolean reverse) {
-        //boolean flag allows shortcut termination if sort is completed before full iteration
-        boolean swapped = true;
-
-        while (swapped) {
+         while (swapped) {
             swapped = false;
 
-            //iterate once for every element
-            for (int i = 0; i < array.length - 1; i++) {
-                //if the current element is larger than the next element...
-                if (array[i] > array[i + 1] && reverse == false || array[i] < array[i + 1] && reverse == true) {
-                    //swap elements
-                    int temp = array[i];
-                    array[i] = array[i + 1];
-                    array[i + 1] = temp;
+             //iterate once for every element
+             for (int i = 0; i < array.length - 1; i++) {
 
-                    //activate flag to quit early
-                    swapped = true;
-                }
-            }
-        }//end while
+                 //if the current element is larger than the next element...
+                 if (array[i] > array[i + 1] && reverse == false || array[i] < array[i + 1] && reverse == true) {
+                     //swap elements
+                     int temp = array[i];
+                     array[i] = array[i + 1];
+                     array[i + 1] = temp;
 
+                     //activate flag to quit early
+                     swapped = true;
+                 }
+             }
+         }//end while
     }
     //overload function to allow default param
     public static void bubbleSort(int[] array) {
-        bubbleSort(array, false);
+         double startTime = System.nanoTime();
+         bubbleSort(array, false);
     }
 
 
 
 
 
-    //selectionSort
+    /**
+     * selectionSort
+     *
+     * @param array
+     * @param reverse [optional]
+     */
     public static void selectionSort(int[] array, boolean reverse) {
         int minPos;
 
@@ -86,7 +100,15 @@ public abstract class Sorts {
 
 
 
-    //insertionSort
+
+
+
+    /**
+     * insertionSort
+     *
+     * @param array
+     * @param reverse [optional]
+     */
     public static void insertionSort(int[] array, boolean reverse) {
         int index;
 
@@ -119,7 +141,15 @@ public abstract class Sorts {
 
 
 
-    //mergeSort
+
+
+
+    /**
+     * mergeSort
+     *
+     * @param array
+     * @param reverse [optional]
+     */
     public static void mergeSort(int[] array, boolean reverse) {
         //determine center
         int pivot = array.length / 2;
@@ -185,18 +215,67 @@ public abstract class Sorts {
 
 
 
+
+    /**
+     * quickSort -- implemented from pseudocode of Tony Hoare's methodology
+     * interesting side note: Tony Hoare invented the null pointer
+     *
+     * @param array
+     * @param reverse [optional]
+     */
+    public static void quickSort(int[] array, int lo, int hi, boolean reverse) {
+        if (lo < hi) {
+            //set new pivot point
+            int pivot = partition(array, lo, hi, reverse);
+
+            //self-invoke for low side and high side
+            quickSort(array, lo, pivot, reverse);
+            quickSort(array, pivot + 1, hi, reverse);
+        }
+    }
+    //overload functions to allow default params
+    public static void quickSort(int[] array, boolean reverse) {
+        quickSort(array, 0, array.length - 1, reverse);
+    }
+    //overload functions to allow default params
     public static void quickSort(int[] array) {
-        
-///////coming soon///////
-
-
-
-
-
-
+        quickSort(array, false);
     }
 
 
+
+    //partition -- helper function for quickSort
+    private static int partition(int[]array, int lo, int hi, boolean reverse) {
+        //capture pivot value for comparison
+        int pivot = array[(hi + lo) / 2];
+
+        //adjust index incrementors
+        lo--;
+        hi++;
+
+        //loop until low and high indexes cross
+        while (true) {
+
+            //iterate through low side until value is found less than pivot value
+            do {
+                lo++;
+            } while (array[lo] < pivot && reverse == false || array[lo] > pivot && reverse == true);
+
+            //iterate through high side until value is greater than pivot value
+            do {
+                hi--;
+            }while (array[hi] > pivot && reverse == false || array[hi] < pivot && reverse == true);
+
+            //done swapping this in this partition
+            if (lo >= hi) return hi;
+
+            //swap sides of identified elements
+            int temp = array[lo];
+            array[lo] = array[hi];
+            array[hi] = temp;
+
+        }//end while
+    }
 
 
 
